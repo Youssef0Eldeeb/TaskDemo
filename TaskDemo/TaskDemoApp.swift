bae8f35a01ef0940f8e9f957b5008aa309b42900
Youssef0Eldeeb <youssefeldeeb3109@gmail.com>
2024-01-07 17:10:02 +0200

    Initial Commit

---
+++ TaskDemo/TaskDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskDemoApp.swift
+//  TaskDemo
+//
+//  Created by Youssef Eldeeb on 07/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
